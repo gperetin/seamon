445f8af699d4d8356313824da01a483163340af4
Goran Peretin <goran.peretin@gmail.com>
2012-03-03 18:00:05 +0100

    implemented persistence (sqlite3) using sqlalchemy

--- server/node_repository_test.py
+++ server/node_repository_test.py
@@ -4,9 +4,15 @@ from hamcrest import *
 from node import Node
 from node_repository import NodeRepository
 
+from db.session import engine
+from db.tables import metadata
+
 class NodeRepositoryTest(unittest.TestCase):
     def setUp(self):
-        NodeRepository.nodes = []
+        metadata.create_all(engine)
+
+    def tearDown(self):
+        metadata.drop_all(engine)
 
     def test_can_store_a_node(self):
         node = Node("node1", "192.123.3.3", 2312)
@@ -14,7 +20,7 @@ class NodeRepositoryTest(unittest.TestCase):
         assert_that(NodeRepository.all()[0], is_(node))
 
     def test_can_find_node_by_name(self):
-        node = Node("node1", "192.123.3.3", 2312)
+        node = Node("node2", "192.123.3.3", 2312)
         NodeRepository.save(node)
-        node_by_name = NodeRepository.by_name("node1")
+        node_by_name = NodeRepository.by_name("node2")
         assert_that(node_by_name, is_(node))
