20cc808c849db3df53090f233a12215fa579df46
Goran Peretin <goran.peretin@gmail.com>
2012-03-03 15:34:05 +0100

    server can identify nodes by name

--- server/seamon_server.py
+++ server/seamon_server.py
@@ -1,14 +1,27 @@
 from data_collector import DataCollector
+from node import Node
 
 class SeamonServer(object):
-    nodes = []
+    def __init__(self):
+        self.nodes = []
 
-    def add_node(self, node):
-        self.nodes.append(node)
+    def add_node(self, name, ip_address, port):
+        print "adding node "+ name
+        self.nodes.append(Node(name, ip_address, port))
 
     def list_nodes(self):
         return self.nodes
 
-    def get_data_from_nodes(self):
+    # def get_data_from_nodes(self):
+    #     for node in self.nodes:
+    #         DataCollector.get_from_node(node)
+
+    def get_node_info(self, node):
+        return DataCollector.get_from_node(node)
+
+    def node_by_name(self, name):
         for node in self.nodes:
-            DataCollector.get_from_node(node)
+            if node.name == name:
+                return node
+
+        return None
--- server/server_test.py
+++ server/server_test.py
@@ -1,28 +1,36 @@
 import unittest
 from hamcrest import *
-from mock import patch
+from mock import patch, Mock
 
 from seamon_server import SeamonServer
 
 class ServerTest(unittest.TestCase):
-    def test_user_can_add_node_to_server(self):
-        server = SeamonServer()
-        server.add_node("node1")
-        server.add_node("node2")
-        nodes = server.list_nodes()
-        assert_that(nodes, only_contains("node1", "node2"))
+    def setUp(self):
+        self.server = SeamonServer()
+
+    def test_has_list_of_nodes(self):
+        self.server.add_node("node1", "192.168.5.2", 2222)
+        self.server.add_node("node2", "194.123.23.3", 2221)
+        nodes = self.server.list_nodes()
+        assert_that(nodes[0].name, is_("node1"))
+        assert_that(nodes[1].name, is_("node2"))
+
+    def test_can_identify_node_by_name(self):
+        self.server.add_node("node1", "192.168.5.5", 22312)
+        node = self.server.node_by_name("node1")
+        assert_that(node.name, is_("node1"))
+        assert_that(node.ip_address, is_("192.168.5.5"))
+        assert_that(node.port, is_(22312))
+
+#     def test_gets_node_info_from_data_collector(self):
+#         server = SeamonServer()
+#         data = server.get_node_info("node1")
 
 
-    @patch("data_collector.DataCollector.get_from_node")
-    def test_server_can_get_data_from_nodes(self, data_collector):
-        server = SeamonServer()
-        server.add_node("node1")
-        server.add_node("node2")
 
-        server.get_data_from_nodes()
-        data_collector.assert_any_call("node1")
-        data_collector.assert_any_call("node2")
+    # def test_returns_none_for_nonexistent_node(self):
+    #     pass
 
 
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#     unittest.main()
