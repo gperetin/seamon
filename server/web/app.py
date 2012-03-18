bc5baf7df2cf6abb369e9820cf84828737a9dcf4
Goran Peretin <goran.peretin@gmail.com>
2012-03-18 16:26:31 +0100

    added GET view for cpu stats

--- server/web/app.py
+++ server/web/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+import json
+
+from server.seamon_server import SeamonServer
+from server.stats_repository import StatsRepository
+
 @app.route("/")
 def index():
     return "list of all registered nodes"
 
+@app.route("/nodes", methods=['POST'])
+def add_node():
+    node_data = json.loads(request.data)
+    SeamonServer.add_node(node_data)
+
+@app.route("/stats/cpu", methods=['GET'])
+def cpu_stats():
+    stats = StatsRepository.get_cpu_stats()
+    for s in stats:
+        s["timestamp"] = s["timestamp"].isoformat()
+    return json.dumps(stats)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
