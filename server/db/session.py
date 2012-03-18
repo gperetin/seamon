661a78ec4382de237f828043bc973701701b1da6
Goran Peretin <goran.peretin@gmail.com>
2012-03-18 11:10:26 +0100

    moved sqlite DB file to new location

--- server/db/session.py
+++ server/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///seamon.db")
+engine = create_engine("sqlite:///server/db/seamon.db")
 Session = sessionmaker(bind=engine)
 session = Session()
